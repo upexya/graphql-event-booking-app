aabd23235f5ba614455fb4e0792c3d34bc322b46
upexya <iupeachya@gmail.com>
2025-04-07 18:25:26 +0100

    setup react and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+  <div className="bg-primary">
+  <p className="text-3xl font-bold underline font-work-sans">hello wold</p>
+  </div>
+  );
+}
+
+export default App;
